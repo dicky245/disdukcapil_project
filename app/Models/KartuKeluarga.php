76f3ba828e5f4d185c2c9dcae55705f4191be325
Benaya <149450519+benaya098@users.noreply.github.com>
2026-04-08 08:04:45 +0700

    .,

--- app/Models/KartuKeluarga.php
+++ app/Models/KartuKeluarga.php
@@ -58,3 +58,4 @@ class KartuKeluarga extends Model
             }
         });
     }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRoles;
 
+    /**
+     * Kolom yang boleh diisi (mass assignment)
+     */
     protected $fillable = [
         'name',
         'username',
@@ -20,55 +22,43 @@ class User extends Authenticatable
         'security_question_answer',
     ];
 
+    /**
+     * Kolom yang disembunyikan
+     */
     protected $hidden = [
         'password',
         'remember_token',
         'security_question_answer',
     ];
 
+    /**
+     * Casting attribute
+     */
     protected $casts = [
         'password' => 'hashed',
     ];
 
     /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
+     * Relasi ke tabel security_questions
      */
-    public $incrementing = false;
-
-    /**
-     * The "type" of the auto-incrementing ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'string';
-
-    /**
-     * Boot function from Laravel.
-     */
-    protected static function boot()
-    {
-        parent::boot();
-        self::creating(function ($model) {
-            if (empty($model->id)) {
-                $model->id = (string) Str::uuid();
-            }
-        });
-    }
-
-    public function username(): string
-    {
-        return 'username';
-    }
-    public function detail_keagamaan()
-    {
-        // User memiliki satu record di tabel keagamaan
-        return $this->hasOne(Keagamaan_Model::class, 'user_id', 'id');
-    }
-
     public function securityQuestion()
     {
         return $this->belongsTo(SecurityQuestion::class);
+    }
+
+    /**
+     * Relasi ke tabel keagamaan
+     */
+    public function detail_keagamaan()
+    {
+        return $this->hasOne(Keagamaan_Model::class, 'user_id', 'id');
+    }
+
+    /**
+     * Override username field untuk login
+     */
+    public function username(): string
+    {
+        return 'username';
     }
 }
